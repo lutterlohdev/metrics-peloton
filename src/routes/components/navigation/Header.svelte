c412a690f2109fae2500dab7c891610821f98099
lutterlohdev <22846656+lutterlohdev@users.noreply.github.com>
2026-03-07 18:18:27 -0600

    added tread metrics banner

--- src/routes/components/navigation/Header.svelte
+++ src/routes/components/navigation/Header.svelte
@@ -7,6 +7,13 @@
   import {getFriendlyDate} from "$lib/utils/dateUtils";
 </script>
 
+<div class="tread-banner">
+  Looking for a similar experience for your Peloton Tread Metrics? Visit <a
+    href="https://tread.lutterloh.dev"
+    target="_blank"
+    rel="noopener noreferrer">Peloton Tread Metrics</a
+  >
+</div>
 <header>
   <div>
     {#if $csvData}
@@ -15,29 +22,55 @@
     {/if}
   </div>
   <div>
-    <h1>Peloton Metrics{#if $csvData}&nbsp;| <span>{$activeWorkoutType}</span>{/if}</h1>
+    <h1>
+      Peloton Metrics{#if $csvData}&nbsp;| <span>{$activeWorkoutType}</span>{/if}
+    </h1>
   </div>
   <div>
     {#if $csvData}
-    <span class="time-period">
-      <span class="sr-only">For dates: </span>{getFriendlyDate($dateFilter.startDate)} - {getFriendlyDate(
-        $dateFilter.endDate
-      )}
-    </span> 
+      <span class="time-period">
+        <span class="sr-only">For dates: </span>{getFriendlyDate($dateFilter.startDate)} - {getFriendlyDate(
+          $dateFilter.endDate
+        )}
+      </span>
       <OptionsButton />
       <OptionsSidebar />
     {/if}
   </div>
 </header>
 
-
-
 <style>
+  .tread-banner {
+    width: 100%;
+    background: #1a5b92;
+    color: white;
+    text-align: center;
+    font-size: 14px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 100000;
+    box-sizing: border-box;
+    height: 35px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .tread-banner a {
+    color: #4cd4eb;
+    text-decoration: underline;
+    font-weight: bold;
+    margin-left: 4px;
+  }
+  .tread-banner a:hover {
+    color: white;
+  }
   header {
     width: 100%;
     display: flex;
     background: #013a63;
     position: fixed;
+    top: 35px;
     z-index: 99999;
     box-shadow: 2px 0px 5px #013a63;
   }
@@ -56,7 +89,7 @@
     padding: 0;
     color: #efefef;
   }
-  h1 > span{
+  h1 > span {
     font-weight: bold;
   }
   .time-period {
@@ -66,7 +99,6 @@
     display: inline-block;
     line-height: 50px;
     vertical-align: bottom;
-
   }
   @media only screen and (max-width: 768px) {
     .time-period {
